02ea368e40ff73136f6e30e35038457e866ac87c
Golo Roden <golo.roden@thenativeweb.io>
2018-06-27 10:25:50 +0200

    Add missing try/catch on top level.

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 
 const path = require('path');
 
-const processEnv = require('processenv'),
+const flaschenpost = require('flaschenpost'),
+      processEnv = require('processenv'),
       tailwind = require('tailwind'),
       WolkenkitApplication = require('wolkenkit-application');
 
@@ -12,89 +13,99 @@ const logic = require('./appLogic'),
       publishEvents = require('./appLogic/publishEvents'),
       repository = require('./repository');
 
+const loggerSystem = flaschenpost.getLogger();
+
 (async () => {
-  const app = tailwind.createApp({
-    profiling: {
-      host: processEnv('PROFILING_HOST'),
-      port: processEnv('PROFILING_PORT')
-    }
-  });
-
-  const applicationDirectory = path.join(app.dirname, 'app');
-  const { writeModel } = new WolkenkitApplication(applicationDirectory);
-
-  await eventStore.initialize({
-    url: app.env('EVENTSTORE_URL'),
-    namespace: `${app.env('APPLICATION')}domain`
-  });
-
-  repository.initialize({ app, writeModel, eventStore });
-
-  await app.eventbus.use(new app.wires.eventbus.amqp.Sender({
-    url: app.env('EVENTBUS_URL'),
-    application: app.env('APPLICATION')
-  }));
-
-  await app.flowbus.use(new app.wires.flowbus.amqp.Sender({
-    url: app.env('FLOWBUS_URL'),
-    application: app.env('APPLICATION')
-  }));
-
-  const eventStream = await eventStore.getUnpublishedEventStream();
-
-  await new Promise((resolve, reject) => {
-    let onData,
-        onEnd,
-        onError;
-
-    const unsubscribe = function () {
-      eventStream.removeListener('data', onData);
-      eventStream.removeListener('end', onEnd);
-      eventStream.removeListener('error', onError);
-    };
-
-    onData = async function (event) {
-      eventStream.pause();
-
-      try {
-        await publishEvents({
-          eventbus: app.eventbus,
-          flowbus: app.flowbus,
-          eventStore,
-          aggregateId: event.aggregate.id,
-          committedEvents: [ event ]
-        });
-      } catch (ex) {
-        return reject(ex);
+  try {
+    const app = tailwind.createApp({
+      profiling: {
+        host: processEnv('PROFILING_HOST'),
+        port: processEnv('PROFILING_PORT')
       }
+    });
 
-      eventStream.resume();
-    };
+    const applicationDirectory = path.join(app.dirname, 'app');
+    const { writeModel } = new WolkenkitApplication(applicationDirectory);
 
-    onEnd = function () {
-      unsubscribe();
-      resolve();
-    };
+    await eventStore.initialize({
+      url: app.env('EVENTSTORE_URL'),
+      namespace: `${app.env('APPLICATION')}domain`
+    });
 
-    onError = function (err) {
-      unsubscribe();
-      reject(err);
-    };
+    repository.initialize({ app, writeModel, eventStore });
 
-    eventStream.on('data', onData);
-    eventStream.on('end', onEnd);
-    eventStream.on('error', onError);
-  });
+    await app.eventbus.use(new app.wires.eventbus.amqp.Sender({
+      url: app.env('EVENTBUS_URL'),
+      application: app.env('APPLICATION')
+    }));
 
-  await app.commandbus.use(new app.wires.commandbus.amqp.Receiver({
-    url: app.env('COMMANDBUS_URL'),
-    application: app.env('APPLICATION')
-  }));
+    await app.flowbus.use(new app.wires.flowbus.amqp.Sender({
+      url: app.env('FLOWBUS_URL'),
+      application: app.env('APPLICATION')
+    }));
 
-  await app.status.use(new app.wires.status.http.Server({
-    port: app.env('STATUS_PORT'),
-    corsOrigin: app.env('STATUS_CORS_ORIGIN')
-  }));
+    const eventStream = await eventStore.getUnpublishedEventStream();
 
-  logic({ app, writeModel, eventStore });
+    await new Promise((resolve, reject) => {
+      let onData,
+          onEnd,
+          onError;
+
+      const unsubscribe = function () {
+        eventStream.removeListener('data', onData);
+        eventStream.removeListener('end', onEnd);
+        eventStream.removeListener('error', onError);
+      };
+
+      onData = async function (event) {
+        eventStream.pause();
+
+        try {
+          await publishEvents({
+            eventbus: app.eventbus,
+            flowbus: app.flowbus,
+            eventStore,
+            aggregateId: event.aggregate.id,
+            committedEvents: [ event ]
+          });
+        } catch (ex) {
+          return reject(ex);
+        }
+
+        eventStream.resume();
+      };
+
+      onEnd = function () {
+        unsubscribe();
+        resolve();
+      };
+
+      onError = function (err) {
+        unsubscribe();
+        reject(err);
+      };
+
+      eventStream.on('data', onData);
+      eventStream.on('end', onEnd);
+      eventStream.on('error', onError);
+    });
+
+    await app.commandbus.use(new app.wires.commandbus.amqp.Receiver({
+      url: app.env('COMMANDBUS_URL'),
+      application: app.env('APPLICATION')
+    }));
+
+    await app.status.use(new app.wires.status.http.Server({
+      port: app.env('STATUS_PORT'),
+      corsOrigin: app.env('STATUS_CORS_ORIGIN')
+    }));
+
+    logic({ app, writeModel, eventStore });
+  } catch (ex) {
+    loggerSystem.fatal('An unexpected error occured.', { err: ex });
+
+    /* eslint-disable no-process-exit */
+    process.exit(1);
+    /* eslint-enable no-process-exit */
+  }
 })();
